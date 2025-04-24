5e125fc62a6e9b31d56e218113d91197ccee44ac
John Briceno <johnteam380@gmail.com>
2025-04-24 18:17:05 -0400

    Añadir body-parser como dependencia explícita

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 // Servidor minimalista para el chat
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 
 // Configuración básica para aceptar JSON y CORS
-app.use(express.json());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
